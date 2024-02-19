9fdad00a206202940de736e3d3e60df3bc1ff20f
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-19 22:47:45 +0100

    import image in APP.JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+//import of items images
+import AbuDhabi from "./assets/img/abudhabi.png"
+import Anas from "./assets/img/anas.png"
+import Bella from "./assets/img/bella.png"
+import Blanc from "./assets/img/blanc.png"
+import Halima from "./assets/img/halima.webp"
+import Imran from "./assets/img/imran.png"
+import Love from "./assets/img/love.png"
+import Makkah from "./assets/img/makkah.webp"
+import Oud from "./assets/img/oud.webp"
+import Royal from "./assets/img/royalGold.webp"
+import Sauvage from "./assets/img/sauvage.webp"
+import Velvet from "./assets/img/velvet.webp"
+
+
+
+
+
 function App() {
 
 
