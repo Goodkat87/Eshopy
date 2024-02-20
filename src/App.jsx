4ac9492080b6cf342f9835644107429841a7d324
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-20 01:26:11 +0100

    update the json and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import { useState } from 'react'
-
-//import of items images
-import AbuDhabi from "./assets/img/abudhabi.png"
-import Anas from "./assets/img/anas.png"
-import Bella from "./assets/img/bella.png"
-import Blanc from "./assets/img/blanc.png"
-import Halima from "./assets/img/halima.webp"
-import Imran from "./assets/img/imran.png"
-import Love from "./assets/img/love.png"
-import Makkah from "./assets/img/makkah.webp"
-import Oud from "./assets/img/oud.webp"
-import Royal from "./assets/img/royalGold.webp"
-import Sauvage from "./assets/img/sauvage.webp"
-import Velvet from "./assets/img/velvet.webp"
+import data from "./assets/json/data.json"
+import Card from './components/Card'
 
 
 
@@ -22,8 +10,8 @@ function App() {
 
 
   return (
-    <div className='w-screen h-screen'>
-        
+    <div className='w-full h-screen flex flex-col items-center'>
+
     </div>
   )
 }
